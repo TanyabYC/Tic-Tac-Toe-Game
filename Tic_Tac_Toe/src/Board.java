17c06cadec10c79fcebb14d535d07a2f4718e688
TanyabYC <tanyab@yourcloud.supply>
2023-04-19 13:34:40 +1200

    White spacing added to class, Board.

--- Tic_Tac_Toe/src/Board.java
+++ Tic_Tac_Toe/src/Board.java
@@ -1,25 +1,35 @@
 import java.awt.*;
 
 public class Board {
+	
 	// grid line width
 	public static final int GRID_WIDTH = 8;
+	
+	
 	// grid line half width
 	public static final int GRID_WIDHT_HALF = GRID_WIDTH / 2;
 	
-	//2D array of ROWS-by-COLS Cell instances
+	
+	// 2D array of ROWS-by-COLS Cell instances
 	Cell [][] cells;
+	
 	
 	/** Constructor to create the game board */
 	public Board() {
 		
-	 //TODO: initialise the cells array using ROWS and COLS constants 
-
+		//TODO: initialise the cells array using ROWS and COLS constants 
+		
 		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
+			
 			for (int col = 0; col < GameMain.COLS; ++col) {
+				
 				cells[row][col] = new Cell(row, col);
+				
 			}
+			
 		}
+		
 	}
 	
 
@@ -29,16 +39,15 @@ public class Board {
 		// TODO: Check whether the game has ended in a draw. 
 		// Hint: Use a nested loop (see the constructor for an example). Check whether any of the cells content in the board grid are Player.Empty. If they are, it is not a draw.
 		// Hint: Return false if it is not a draw, return true if there are no empty positions left
-		   
-		
-
 		
 	}
 	
 	/** Return true if the current player "thePlayer" has won after making their move  */
 	public boolean hasWon(Player thePlayer, int playerRow, int playerCol) {
+		
 		 // check if player has 3-in-that-row
-		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
+		if (cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
+			
 			return true; 
 		
 		 // TODO: Check if the player has 3 in the playerCol.
@@ -47,7 +56,8 @@ public class Board {
 		
 		
 		 // 3-in-the-diagonal
-		if( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
+		if ( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
+			
 			return true;
 		 
 		
@@ -57,6 +67,7 @@ public class Board {
 		
 		//no winner, keep playing
 		return false;
+		
 	}
 	
 	/**
@@ -64,26 +75,41 @@ public class Board {
 	 * Cells to paint themselves into the grid
 	 */
 	public void paint(Graphics g) {
+		
 		//draw the grid
 		g.setColor(Color.gray);
-		for (int row = 1; row < GameMain.ROWS; ++row) {          
-			g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDHT_HALF,                
-					GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,                
-					GRID_WIDTH, GRID_WIDTH);       
-			}
-		for (int col = 1; col < GameMain.COLS; ++col) {          
-			g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDHT_HALF, 0,                
-					GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,                
+		
+		for (int row = 1; row < GameMain.ROWS; ++row) {
+			
+			g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDHT_HALF,
+					
+					GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,
+					
 					GRID_WIDTH, GRID_WIDTH);
+			
+		}
+		
+		for (int col = 1; col < GameMain.COLS; ++col) {
+			
+			g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDHT_HALF, 0,
+					
+					GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,
+					
+					GRID_WIDTH, GRID_WIDTH);
+			
 		}
 		
 		//Draw the cells
-		for (int row = 0; row < GameMain.ROWS; ++row) {          
-			for (int col = 0; col < GameMain.COLS; ++col) {  
+		for (int row = 0; row < GameMain.ROWS; ++row) {
+			
+			for (int col = 0; col < GameMain.COLS; ++col) {
+				
 				cells[row][col].paint(g);
+				
 			}
+			
 		}
+		
 	}
-	
 	
 }
