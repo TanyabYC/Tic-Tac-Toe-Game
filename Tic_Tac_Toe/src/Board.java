c04a0b3350d5773715b5eb894e0476c76442bbad
TanyabYC <tanyab@yourcloud.supply>
2023-04-19 13:48:35 +1200

    Functionality for when a player wins after making a move updated.

--- Tic_Tac_Toe/src/Board.java
+++ Tic_Tac_Toe/src/Board.java
@@ -65,20 +65,24 @@ public class Board {
 		
 		 // TODO: Check if the player has 3 in the playerCol.
 		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		
+		if (cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
+			
+			return true;
 		
 		
 		 // 3-in-the-diagonal
-		if ( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
+		if (cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
 			
 			return true;
 		 
 		
 		// TODO: Check the diagonal in the other direction
-		
+		if (cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer)
+			
+			return true;
 
 		
-		//no winner, keep playing
+		// no winner, keep playing
 		return false;
 		
 	}
